b85c3b160703aeb6114ec4bfb202e3ed5c693462
marl0ny <52328879+marl0ny@users.noreply.github.com>
2020-03-16 13:15:01 -0400

    Add files via upload

---
+++ simple-wave.js
@@ -0,0 +1,71 @@
+/*
+* Simple numerical simulation of the 1D wave equation.
+*
+* The the 1D wave equation can be discretized as
+*   u(x, t + dt) = (c dt/dx)(u(x + dx, t) - 2u(x, t) + u(x - dx, t)) + u(t, t)
+* Where dt and dx are the time and spatial steps, and c is the propagation speed
+* of the wave. In the case when c dt/ dx = 1, the above equation reduces to
+*  u(x, t + dt) = u(x + dx, t) - u(x, t) + u(x - dx, t),
+* which is an exact solution, at least for this particular case 
+* (Gould et al. 2007, pg 342).
+*
+* References:
+*  Gould, H., Tobochnik J., Christian W. (2007). Normal Modes and Waves.
+*  In An Introduction to Computer Simulation Methods, 
+*  chapter 9. Pearson Addison-Wesley.
+*
+*/
+
+
+let canvas = document.getElementById("sketchCanvas");
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight;
+let ctx = canvas.getContext("2d");
+let wave = [0, 1, 2, 3].map(elem => new Array(canvas.width).fill(0.0));
+// wave[0]: wave amplitudes at last time step
+// wave[1]: current time step
+// wave[2]: next time step
+// wave[3]: Store a new wave shape as dictated by user input
+
+function mouseChangeWave(event) {
+    if (event.buttons !== 0) {
+        let x = event.clientX - canvas.offsetLeft;
+        let y = canvas.height/2 - (event.clientY - canvas.offsetTop);
+        for (var i = -Math.floor(canvas.width/8)+1; i < canvas.width/8; i++) {
+            wave[3][x+i] = ((x + i) > 0 && (x + i) < canvas.width - 1)? 
+            y*Math.exp(-1.0*i*i/(Math.sqrt(2.0)*canvas.width/30.0)**2): wave[3][x+i];
+        }
+    }
+}
+document.addEventListener("ontouchmove", mouseChangeWave);
+document.addEventListener("mousemove", mouseChangeWave);
+
+
+document.addEventListener("mouseup", event => {
+    for (let i = 0; i < wave[0].length; i++) {
+        wave[0][i] += wave[3][i];
+        wave[1][i] += wave[3][i];
+        wave[3][i] = 0.0;
+    }
+});
+
+
+function animate() {
+    for (let i = 1; i < wave[0].length - 1; i++) {
+        wave[2][i] = wave[1][i + 1] + wave[1][i - 1] - wave[0][i];
+    }
+    ctx.clearRect(0.0, 0.0, canvas.clientWidth, canvas.clientHeight);
+    ctx.beginPath();
+    ctx.strokeStyle = 'rgba(255, 204, 102, 1)'; 
+    ctx.moveTo(0, canvas.height/2 - wave[1][0] - wave[3][0]);
+    for (let i = 0; i < canvas.width; i++) {
+        ctx.lineTo(i, canvas.height/2 - wave[1][i] - wave[3][i]);
+        ctx.moveTo(i, canvas.height/2 - wave[1][i] - wave[3][i]);
+        wave[3][i] = 0.0;
+    }
+    ctx.stroke();
+    ctx.closePath();
+    wave = [wave[1], wave[2], wave[0], wave[3]];
+    requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
---
+++ simple-wave2d.js
@@ -0,0 +1,77 @@
+/*
+* Waves in 2D.
+*/
+
+function createGrid(n, m) {
+    let grid = [];
+    for (let i = 0; i < n; i++) {
+        grid.push(new Array(m).fill(0.0));
+    }
+    return grid;
+}
+
+
+let canvas = document.getElementById("sketchCanvas");
+let ctx = canvas.getContext("2d");
+canvas.width = 600;
+canvas.height = 600;
+let grid2Canvas = 5;
+let gridLength = Math.round(canvas.width/grid2Canvas);
+let amp = 230; // Amplitude of the wave
+let wave = [0, 1, 2, 3].map(elem => createGrid(gridLength, gridLength));
+// wave[0]: Store a new wave shape as dictated by user input
+// wave[1]: wave amplitudes at last time step
+// wave[2]: current time step
+// wave[3]: next time step
+
+
+function mouseChangeWave(event) {
+    if (event.buttons !== 0) {
+        let x = Math.floor((event.clientX - canvas.offsetLeft)/grid2Canvas);
+        let y = Math.floor((event.clientY - canvas.offsetTop)/grid2Canvas);
+        for (var i = -Math.floor(wave[0].length/8)+1; i < wave[0].length/8; i++) {
+            for (var j = -Math.floor(wave[0].length/8)+1; j < wave[0].length/8; j++) {
+                if (((x + i) > 0 && (x + i) < wave[0].length - 1) && 
+                    ((y + j) > 0 && (y + j) < wave[0][0].length - 1)) {
+                    wave[0][x+i][y+j] = (
+                        amp*Math.exp(-1.0*(i*i + j*j)/(Math.sqrt(2.0)*wave[0].length/50.0)**2)
+                    );
+                }
+            }
+        }
+    }
+}
+
+
+document.addEventListener("ontouchmove", mouseChangeWave);
+document.addEventListener("mousemove", mouseChangeWave);
+document.addEventListener("mouseup", event => {
+    for (var i = 0; i < wave[0].length; i++) {
+        for (var j = 0; j < wave[0][0].length; j++) {
+            wave[1][i][j] += wave[0][i][j];
+            wave[2][i][j] += wave[0][i][j];
+            wave[0][i][j] = 0.0;
+        }
+    }
+});
+
+
+function animate() {
+    let col = 0;
+    for (let i = 1; i < wave[0].length - 1; i++) {
+        for (let j = 1; j < wave[0][0].length - 1; j++) {
+            wave[3][i][j] = 0.5*(wave[2][i+1][j] + wave[2][i-1][j] + 
+                                 wave[2][i][j+1] + wave[2][i][j-1]) - wave[1][i][j];
+            col = Math.floor(wave[2][i][j] + wave[0][i][j]);
+            col += 25;
+            ctx.fillStyle = `rgb(${Math.floor(col/4)}, ${Math.floor(col/2)}, ${col})`;
+            ctx.fillRect(i*grid2Canvas, j*grid2Canvas, 
+                            grid2Canvas, grid2Canvas);
+            wave[0][i][j] = 0.0;
+        }
+    }
+    wave = [wave[0], wave[2], wave[3], wave[1]];
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
